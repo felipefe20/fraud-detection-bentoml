f4587dd7e96bffe6b54671c18eaf2eb74e37a52b
Oscar Felipe Fernandez <ofelipefernandez@gmail.com>
2024-01-31 21:28:11 -0500

    dvc setup

--- fraud-detection/src/process.py
+++ fraud-detection/src/process.py
@@ -6,7 +6,7 @@ Author: Khuyen Tran
 
 import hydra
 from omegaconf import DictConfig
-
+import pandas as pd
 
 @hydra.main(config_path="../config", config_name="main", version_base=None)
 def process_data(config: DictConfig):
@@ -14,6 +14,10 @@ def process_data(config: DictConfig):
 
     print(f"Process data using {config.data.raw}")
     print(f"Columns used: {config.process.use_columns}")
+    df=pd.read_csv(config.data.raw)
+    df.drop_duplicates(inplace=True)
+    df= df.drop('Time', axis=1)
+    numeric_columns = (list(df.loc[:, 'V1':'Amount']))
 
 
 if __name__ == "__main__":
--- fraud-detection/src/train_model.py
+++ fraud-detection/src/train_model.py
@@ -6,6 +6,16 @@ Author: Khuyen Tran
 
 import hydra
 from omegaconf import DictConfig
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+
+
+
 
 
 @hydra.main(config_path="../config", config_name="main", version_base=None)
@@ -16,6 +26,6 @@ def train_model(config: DictConfig):
     print(f"Model used: {config.model.name}")
     print(f"Save the output to {config.data.final}")
 
-
+    data=pd.read_csv(config.data.processed)
 if __name__ == "__main__":
     train_model()
